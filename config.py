1b8c1a52693eb68870849ace18f7110abf9eacc0
himanshuforigpage-dev <himanshuforigpage@gmail.com>
2025-12-29 19:44:06 +0530

    Update configuration values in config.py
    Updated BOT_TOKEN, API_ID, and API_HASH in config.py.

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 ##Code Written By @ItsMeMaster
 
 class Config(object):
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8457341970:AAEL7ez7cFFWDOx2n1BKiuYCUJwvibCK4U4"
     DB_NAME = "takkishor9784"
-    API_ID = "21157244"
-    API_HASH = "4981c2699bd91c7db836ec8f77e5b0f0"
+    API_ID = "21025319"
+    API_HASH = "cfc533caac028e0e284dae2d571ad5b0"
     ADMIN_ID = [1783306092]
     DB_URL = "mongodb+srv://takkishor9784:gG73juoh44MnvJEZ@cluster0.q8hxdk2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     LOG_CHANNEL = -1002758652479 # Your Log Channel ID (Bot ko ADMIN BNAYE)
